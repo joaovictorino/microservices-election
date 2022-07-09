5fde62083d8052e73f973cc67538dde1e7aeeb7a
joaovictorino <jhvictorino@gmail.com>
2022-07-09 01:11:41 +0000

    proxy

--- VotesAPI/Program.cs
+++ VotesAPI/Program.cs
@@ -36,17 +36,13 @@ builder.Services.AddScoped<CandidatesIntegration>();
 
 var app = builder.Build();
 
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 using(var scope = app.Services.CreateScope()){
     var db = scope.ServiceProvider.GetRequiredService<DataContext>();
     db.Database.Migrate();
 }
-
 
 app.UseHttpsRedirection();
 
