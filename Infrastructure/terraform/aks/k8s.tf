649cd324ea952b368150a80450f457e3db3c94a8
joaovictorino <jhvictorino@gmail.com>
2022-07-31 05:30:46 +0000

    kube config

--- Infrastructure/terraform/aks/k8s.tf
+++ Infrastructure/terraform/aks/k8s.tf
@@ -1,9 +1,13 @@
 provider "kubernetes" {
-  host                   = data.azurerm_kubernetes_cluster.default.kube_config.0.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.bootcamp-aks.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.bootcamp-aks.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.bootcamp-aks.kube_config.0.cluster_ca_certificate)
+  host                   = data.azurerm_kubernetes_cluster.default.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.bootcamp-aks.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.bootcamp-aks.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.bootcamp-aks.kube_config[0].cluster_ca_certificate)
 }
+
+# provider "kubernetes" {
+#   config_path = "~/.kube/config"  
+# }
 
 resource "kubernetes_namespace" "bootcamp" {
   metadata {
