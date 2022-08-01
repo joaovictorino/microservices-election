3dc682ff6b79fba13a6d723b8530d508202de63a
joaovictorino <jhvictorino@gmail.com>
2022-07-31 23:34:36 -0300

    config

--- Infrastructure/terraform/aks/k8s.tf
+++ Infrastructure/terraform/aks/k8s.tf
@@ -7,13 +7,13 @@ metadata:
   name: bootcamp-configmap
   namespace: bootcamp
 data:
-  ASPNETCORE_Azure: true
-  ASPNETCORE_CandidateDatabase__ConnectionString: ${module.solution.azurerm_cosmosdb_account.connection_strings[0]}
-  ASPNETCORE_CandidateDatabase__DatabaseName: Candidates
-  ASPNETCORE_ConnectionStrings__VotesDatabase: Server=tcp:${module.solution.azurerm_mssql_server.fully_qualified_domain_name},1433;Initial Catalog=${module.solution.azurerm_mssql_database.name};Persist Security Info=False;User ID=${module.solution.azurerm_mssql_server.administrator_login};Password=${module.solution.azurerm_mssql_server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
-  ASPNETCORE_ENVIRONMENT: Production
-  ASPNETCORE_Integrations__CandidateAddress: http://candidates-svc/api/candidates/
-  ASPNETCORE_RabbitMQ: Endpoint=${module.solution.azurerm_servicebus_namespace.default_primary_connection_string}
+  ASPNETCORE_Azure: "true"
+  ASPNETCORE_CandidateDatabase__ConnectionString: "${module.solution.azurerm_cosmosdb_account.connection_strings[0]}"
+  ASPNETCORE_CandidateDatabase__DatabaseName: "Candidates"
+  ASPNETCORE_ConnectionStrings__VotesDatabase: "Server=tcp:${module.solution.azurerm_mssql_server.fully_qualified_domain_name},1433;Initial Catalog=${module.solution.azurerm_mssql_database.name};Persist Security Info=False;User ID=${module.solution.azurerm_mssql_server.administrator_login};Password=${module.solution.azurerm_mssql_server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  ASPNETCORE_ENVIRONMENT: "Production"
+  ASPNETCORE_Integrations__CandidateAddress: "http://candidates-svc/api/candidates/"
+  ASPNETCORE_RabbitMQ: "Endpoint=${module.solution.azurerm_servicebus_namespace.default_primary_connection_string}"
 EOF
     depends_on = [ 
       null_resource.get_credentials,
